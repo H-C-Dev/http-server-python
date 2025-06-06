dda40b3b63b7d06a38288062c7de633f2a56457f
H-C-Dev <th.chan.dev2024@gmail.com>
2025-06-06 15:36:41 +0100

    feat: create CustomSocket, CustomRequest, CustomResponse, and Server class to encapsulate logic to handle simple HTTP/1.1 request from a client

--- server.py
+++ server.py
@@ -1,22 +1,79 @@
 import socket
 PORT=8080
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), PORT))
-s.listen(5)
 
-print(f"Listening on port: {PORT}")
+class CustomSocket:
+    def __init__(self, port: int, backlog: int = 5, host: str = socket.gethostname()):
+        self.host = host
+        self.port = port
+        self.backlog = backlog
+        self.socket = None
 
-while True:
-    (client_socket, client_address) = s.accept()
-    print(f"Got a connection from {client_address}")
-    request = client_socket.recv(4096).decode('utf-8')
-    print(request)
-    response = (
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/plain; charset=utf-8\r\n"
-        "Content-Length: 37\r\n"
-        "\r\n"
-        "This is a test for HTTP/1.1 Response!"
-    )
-    client_socket.sendall(response.encode('utf-8'))
-    client_socket.close()
+    def __createSocket(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket = s
+        print(f"A socket has been created, will now try to bind it to {self.host} with port: {self.port}")
+
+    def __bindPort(self):
+        self.socket.bind((self.host, self.port))
+
+    def __listenPort(self):
+        self.socket.listen(self.backlog)
+
+    def acceptConnection(self):
+        (client_socket, client_address) = self.socket.accept()
+        return (client_socket, client_address)
+    
+    def createAndBindSocket(self):
+        self.__createSocket()
+        self.__bindPort()
+        self.__listenPort()
+
+class CustomRequest:
+    def __init__(self, socket: socket.socket, bufsize: int = 4096, encoding: str = 'utf-8'):
+        self.socket = socket
+        self.bufsize = bufsize
+        self.encoding = encoding
+        self.val = None
+    
+    def parseRequest(self) -> any: 
+        return self.socket.recv(self.bufsize).decode(self.encoding)
+
+class CustomResponse:
+    def __init__(self, message: str, encoding: str = 'utf-8'):
+        self.val = (
+            "HTTP/1.1 200 OK\r\n"
+            f"Content-Type: text/plain; charset={encoding}\r\n"
+            f"Content-Length: {len(message)}\r\n"
+            "\r\n"
+            f"{message}"
+        )
+    
+    def encode(self, encoding: str = 'utf-8'):
+        return self.val.encode(encoding)
+
+
+
+class Server:
+    def __init__(self, port: int = PORT, host: str = socket.gethostname(), backlog: int = 5):
+        self.port = port
+        self.host = host
+        self.backlog = backlog
+    
+    def initServer(self):
+        s = CustomSocket(self.port, self.backlog, self.host)
+        s.createAndBindSocket()
+        print(f"Listening on port: {self.port}")
+        while True:
+            (client_socket, client_address) = s.acceptConnection()
+            print(f"Got a connection from {client_address}")
+            request = CustomRequest(client_socket)
+            print(request.parseRequest())
+            response = CustomResponse("Welcome to my Chango server")
+            client_socket.sendall(response.val.encode('utf-8'))
+            client_socket.close()
+    
+
+
+server = Server()
+server.initServer()
+
