f7864c0cfbb341420c4ca23333b871bf3553b73c
H-C-Dev <th.chan.dev2024@gmail.com>
2025-06-12 23:48:27 +0100

    refactor: refactored the server class to HTTPServer and Server classes using template pattern

--- server.py
+++ server.py
@@ -1,171 +1,75 @@
-import socket
-from urllib.parse import parse_qs, unquote_plus
+from custom_socket import CustomSocket
+from http_error import HTTPError, MethodNotAllowed, InternalServerError
+from constants import ContentType
+from request import CustomRequest
+from response import CustomResponse
+
 PORT=8080
 
-class CustomSocket:
-    def __init__(self, port: int, backlog: int = 5, host: str = socket.gethostname()):
+class HTTPServer:
+    def __init__(self, host, port, backlog=5):
         self.host = host
         self.port = port
         self.backlog = backlog
-        self.val = None
 
-    def __createSocket(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.val = s
-        print(f"A socket has been created, will now try to bind it to {self.host} with port: {self.port}")
-
-    def __bindPort(self):
-        self.val.bind((self.host, self.port))
-
-    def __listenPort(self):
-        self.val.listen(self.backlog)
-
-    def acceptConnection(self):
-        (clientSocket, clientAddress) = self.val.accept()
-        return (clientSocket, clientAddress)
+    def __create_socket(self) -> CustomSocket:
+        server_socket = CustomSocket(self.port, self.backlog, self.host)
+        server_socket.create_and_bind_socket()
+        print(f"Listening on {self.host}:{self.port}")
+        return server_socket
     
-    def createAndBindSocket(self):
-        self.__createSocket()
-        self.__bindPort()
-        self.__listenPort()
-
-class CustomRequest:
-    def __init__(self, bufsize: int = 4096, encoding: str = 'utf-8'):
-        self.bufsize = bufsize
-        self.encoding = encoding
-
-    def __receiveHeaderLine(self, clientSocket: socket.socket):
-        # empty byte
-        data = b""
-        while b"\r\n\r\n" not in data:
-            chunk = clientSocket.recv(self.bufsize)
-            # if we no longer receive any chunk, also end the loop
-            if not chunk:
-                break
-            data += chunk
-        
-        header, space, body = data.partition(b"\r\n\r\n")
-        headerText = header.decode(self.encoding, errors="ignore")
-        lines = headerText.split("\r\n")
-        return (lines, body)
-    
-    def __extracrtRequestLine(self, lines):
-        return lines[0].split(" ")
-    
-    def __parseHeaders(self, lines):
-        headers = dict()
-        for line in lines[1:]:
-            if not line:
-                continue
-            name, value = line.split(":", 1)
-            headers[name.strip().lower()] = value.lstrip()
-
-        return headers
-    
-    def __extractBody(self, body, headers, clientSocket: socket.socket):
-        contentLength = int(headers.get("content-length", "0"))
-        # carry on receiving the rest of the TCP packets if the length is bigger than what we received
-        while len(body) < contentLength:
-            body += clientSocket.recv(self.bufsize)
-        return body
-    
-    def __extractPathAndQuery(self, rawPath):
-        # if there is a "?", parse the query
-        if "?" in rawPath:
-            path, qs = rawPath.split("?", 1)
-            query = parse_qs(qs, keep_blank_values=True)
-            return (path, query)
-        else:
-            path = rawPath
-            query = {}
-            return (path, query)
-
-    
-    def parseRequest(self, clientSocket: socket.socket) -> any:
-        lines, body = self.__receiveHeaderLine(clientSocket)
-        # http method, path and http version in the requestLine
-        method, path, version = self.__extracrtRequestLine(lines)
-        headers = self.__parseHeaders(lines)
-        body = self.__extractBody(body, headers, clientSocket)
-        path, query = self.__extractPathAndQuery(path)
-
-        return {
-            "method": method,
-            "path": unquote_plus(path),
-            "version": version,
-            "query": query,
-            "body": body,
-            "headers": headers
-        }
-
-
-httpStatusCode = {
-    200: "OK",
-    404: "Not Found"
-}
-
-contentType = {
-    "application/json":"application/json",
-    "text/plain":"text/plain"
-    
-    }
-
-
-class CustomResponse:
-    def __init__(self, message: str, contentType, statusCode, encoding: str = 'utf-8'):
-        self.statusCode = statusCode
-        self.encoding = encoding
-        self.contentType = contentType
-        self.message = message
-        self.statusLine = None
-        self.val = None
-
-    def constructResponse(self) -> bytes:
-        if self.statusCode in httpStatusCode:
-            self.statusLine = f"HTTP/1.1 {self.statusCode} {httpStatusCode[self.statusCode]}\r\n"
-            response = (
-                f"{self.statusLine}"
-                f"Content-Type: {self.contentType}; charset={self.encoding}\r\n"
-                f"Content-Length: {len(self.message)}\r\n"
-                "\r\n"
-                f"{self.message}"
-            )
-            self.val = response
-            formattedRes = self.__encode()
-            return formattedRes
-        else:
-            raise Exception("Status code not found")
-
-
-    def __encode(self):
-        return self.val.encode(self.encoding)
-
-
-
-class Server:
-    def __init__(self, port: int = PORT, host: str = socket.gethostname(), backlog: int = 5):
-        self.port = port
-        self.host = host
-        self.backlog = backlog
-    
-    def initServer(self):
-        socket = CustomSocket(self.port, self.backlog, self.host)
-        socket.createAndBindSocket()
-        print(f"Listening on port: {self.port}")
+    def __enter_accept_state(self, server_socket: CustomSocket):
         while True:
-            (clientSocket, clientAddress) = socket.acceptConnection()
-            print(f"Got a connection from {clientAddress}")
-            request = CustomRequest()
-            print(request.parseRequest(clientSocket))
-            response = CustomResponse("Welcome to my Hango server", "text/plain", 200)
-            print(response.constructResponse())
-            clientSocket.sendall(response.constructResponse())
-            clientSocket.close()
-        
-        
+            (client_socket,  client_address) = server_socket.accept_connection()
+            print(f"Got request from IP: {client_address}")
+            try:
+                request = self.parse_request(client_socket)
+                response = self.handle_request(request)
+            except HTTPError as http_error:
+                response = self.handle_error(http_error)
+            except Exception as e:    
+                print("Unexpected:", e)
+                response = self.handle_error(InternalServerError)
+            client_socket.sendall(response.construct_response())
+            client_socket.close()
 
+    def start_server(self):
+        server_socket = self.__create_socket()
+        self.__enter_accept_state(server_socket)
+
+    def parse_request(self, client_socket):
+        raise NotImplementedError
+
+    def handle_request(self, request):
+        raise NotImplementedError
+
+    def handle_error(self, http_error):
+        return CustomResponse(
+            http_error.message,
+            ContentType.PLAIN,
+            http_error.status_code
+        )
+
+class Server(HTTPServer):
+    def parse_request(self, client_socket):
+        return CustomRequest().parse_request(client_socket)
+    
+    def handle_request(self, request):
+        method, path = request['method'], request['path']
+        if method == 'GET':
+            body = f"GET {path}"
+            return CustomResponse(body, ContentType.PLAIN, 200)
         
+        elif method == 'POST':
+            body = request['body'].decode(request.get('encoding', 'utf-8'))
+            return CustomResponse(f"POST: {body}", ContentType.PLAIN, 200)
+        
+        else:
+            raise MethodNotAllowed(f"{method} - Method Not Allowed")
 
-server = Server(8080, "0.0.0.0")
-server.initServer()
 
+def main():
+    server = Server("0.0.0.0", 8080)
+    server.start_server()
+
+main()
