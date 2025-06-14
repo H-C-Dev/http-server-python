6a10893d164d8aab484fa033a341801964f169a5
H-C-Dev <th.chan.dev2024@gmail.com>
2025-06-14 17:27:56 +0100

    fix: added json.dumps to ensure list and dict type are serialised to JSON str before encoding

--- response.py
+++ response.py
@@ -1,7 +1,8 @@
-from constants import http_status_codes_message
+from constants import http_status_codes_message, ContentType
+import json
 
 class CustomResponse:
-    def __init__(self, body: str, content_type, status_code, encoding: str = 'utf-8'):
+    def __init__(self, body: str, status_code: str, content_type: str = ContentType.PLAIN.value, encoding: str = 'utf-8'):
         self.status_code = status_code
         self.encoding = encoding
         self.content_type = content_type
@@ -10,23 +11,35 @@ class CustomResponse:
         self.header_block = None
 
     def __encode_body(self):
-        return self.body.encode(self.encoding)
+        if isinstance(self.body, (dict, list)):
+            text = json.dumps(self.body)
+        else:
+            text = str(self.body)
+        return text.encode(self.encoding)
         
     def __encode_and_combine_response(self):
         return self.status_line.encode(self.encoding) + self.header_block.encode(self.encoding) + self.__encode_body()
     
-    def construct_response(self) -> bytes:
-        if self.status_code in http_status_codes_message:
-            body_len = len(self.__encode_body())
-            self.status_line = f"HTTP/1.1 {self.status_code} {http_status_codes_message[self.status_code]}\r\n"
-            self.header_block = (
+    def __set_status_line(self):
+        self.status_line = f"HTTP/1.1 {self.status_code} {http_status_codes_message[self.status_code]}\r\n"
+    
+    def __set_header_block(self):
+        body_len = len(self.__encode_body())
+        self.header_block = (
                 f"Content-Type: {self.content_type}; charset={self.encoding}\r\n"
                 f"Content-Length: {body_len}\r\n"
                 "\r\n"
             )
+    
+    def construct_response(self) -> bytes:
+        if self.status_code in http_status_codes_message:
+            self.__set_status_line()
+            self.__set_header_block()
             return self.__encode_and_combine_response()
         else:
             raise Exception("Status code not found")
         
 
-
+class CustomJSONResponse(CustomResponse):
+    def __init__(self, body: str, status_code: str, encoding: str = 'utf-8'):
+        super().__init__(body, status_code,  content_type=ContentType.JSON.value, encoding=encoding)
