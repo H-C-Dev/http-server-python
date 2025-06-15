d7cd628d0ce2040ee4b291b6c2ee5b05844efb83
H-C-Dev <th.chan.dev2024@gmail.com>
2025-06-15 23:17:08 +0100

    test: created test for extract_raw_path_and_method

--- test_server.py
+++ test_server.py
@@ -2,8 +2,15 @@ import pytest
 from server import HTTPServer, Server
 from response import CustomResponse
 
-def test__invoke_handler():
-    server = Server("0.0.0.0", 8080)
+@pytest.fixture
+def server():
+    return Server("0.0.0.0", 8080)
+
+@pytest.fixture
+def http_server():
+    return HTTPServer("0.0.0.0", 8080)
+
+def test__invoke_handler(server):
     mock_value = "testing..."
 
     def mock_handler(text):
@@ -17,3 +24,50 @@ def test__invoke_handler():
     assert actual_response.body == expected_response.body
     assert actual_response.status_line == expected_response.status_line
     assert actual_response.header_block == expected_response.header_block
+
+
+
+
+def test__extract_raw_path_and_method(server):
+    method = "POST"
+    path = "/test"
+    mock_request = {"method": method, "path": path}
+
+    (actual_method, actual_path) = server._Server__extract_raw_path_and_method(mock_request)
+    expected_method = method
+    expected_path = path
+
+    assert actual_method == expected_method
+    assert actual_path == expected_path
+
+
+def test__enter_accept_state_one_iteration(mocker, http_server):
+    mock_socket = mocker.Mock()
+    mock_client_socket = mocker.Mock()
+    mock_client_address = ("127.0.0.1", 65535)
+
+
+    # [Learning]: use side_effect to manipulate the accept_connection behaviour
+    mock_socket.accept_connection.side_effect = [
+        (mock_client_socket, mock_client_address),
+        # pretend to raise a Exception from accept_connection to terminate the accept state loop
+        Exception("Terminate accept state loop")
+    ]
+
+    # [Learning]: First arg: class, Second arg: method name in str form, Third arg: simulate expected val
+    mocker.patch.object(http_server, "parse_request", return_value={"method": "GET", "path": "/"})
+
+    # mock the CustomResponse for the sake of isolation
+    mock_response = mocker.Mock(spec=CustomResponse)
+    mock_response.construct_response.return_value = b"testing"
+    mocker.patch.object(http_server, "handle_request", return_value=mock_response)
+
+    # [Learning]: use pytest.raises to raise the Exception to break out of infinite loop.
+    with pytest.raises(Exception, match="Terminate accept state loop"):
+        http_server._HTTPServer__enter_accept_state(mock_socket)
+
+    # [Learning]: assert_called -> make sure the method has been called
+    mock_socket.accept_connection.assert_called()
+    # [Learning]: assert_called_with -> it called the method with the byte: "testing"
+    mock_client_socket.sendall.assert_called_with(b"testing")
+    mock_client_socket.close.assert_called()
