40e780911517ba0660e2b2d425a712f9c3a162c1
H-C-Dev <th.chan.dev2024@gmail.com>
2025-06-19 13:52:21 +0100

    refactor: abstract server object

--- __main__.py
+++ __main__.py
@@ -1,16 +1,8 @@
-from server import Server
+from server import server
 from response import CustomResponse
-from handler import return_hello_world, return_client_data, post_endpoint
-def main():
-    server = Server("0.0.0.0", 8080)
-    def catch_favicon():
-        return CustomResponse(body="a favicon is detected", status_code=200)
-    server.GET("/favicon.ico", catch_favicon)
-    server.GET("/test", return_hello_world)
-    server.GET("/data/{client_data}", return_client_data)
-    server.POST("/post", post_endpoint)
-    server.start_server()
+import handler
 
-    
-    
+def main():
+    server.start_server()   
+
 main()
