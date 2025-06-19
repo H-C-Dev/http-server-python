a4285ad2b4da38495f2bf37f73c6b4223f53e18c
H-C-Dev <th.chan.dev2024@gmail.com>
2025-06-19 13:53:52 +0100

    feat: added type-safe to handle functions

--- handler.py
+++ handler.py
@@ -1,13 +1,30 @@
-from response import CustomResponse
+from response   import CustomResponse
+from type_safe  import type_safe
+from server import server
 
+@server.GET("/favicon.ico")
+@type_safe
+def catch_favicon() -> CustomResponse:
+    return CustomResponse(body="a favicon is detected", status_code=200)
+
+@server.GET("/test")
+@type_safe
 def return_hello_world() -> CustomResponse:
-    message = "hello world"
-    return CustomResponse(body=message, status_code=200)
+    return CustomResponse(body="hello world", status_code=200)
 
-def return_client_data(client_data: str) -> str:
+@server.GET("/data/{client_data}")
+@type_safe
+def return_client_data(client_data: dict) -> CustomResponse:
+    res = test_safe("hello")
+    print(res)
     return CustomResponse(body=client_data, status_code=200)
 
-def post_endpoint(post_data: str) -> str:
-    print("data received from client: ", post_data)
-    print("post endpoint has been invoked")
+@server.POST("/post")
+@type_safe
+def post_endpoint(post_data: str) -> CustomResponse:
+    print("data received from client:", post_data)
     return CustomResponse(body="hello world from post endpoint", status_code=200)
+
+@type_safe
+def test_safe(foo: str) -> str:
+    return foo
