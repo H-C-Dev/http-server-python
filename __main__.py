5cfa305f2da7bed7d1d13f0f8e6c21eb66490c2b
H-C-Dev <th.chan.dev2024@gmail.com>
2025-07-07 17:33:31 +0100

    Chore: Added print message to show the server is running

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ from server import server
 import handler
 async def main():
     server_obj = await server.init_server()
+    print("Server is up and running.")
     loop = asyncio.get_running_loop()
     loop.add_signal_handler(signal.SIGINT, server_obj.close)
     await server_obj.wait_closed()
