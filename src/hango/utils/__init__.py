0392af5fba98933cb87e44a04ab6bba2918470a5
H-C-Dev <th.chan.dev2024@gmail.com>
2025-07-11 17:58:45 +0100

    Feature: Created HandleEarlyHintsResponse to handle extract the header info that is related to early hints response, and construct the hints which provides early hints information in the 103 http response

--- src/hango/utils/__init__.py
+++ src/hango/utils/__init__.py
@@ -2,6 +2,6 @@ from .type_safe import type_safe
 from .extract_params import ExtractParams
 from .server_file import ServeFile
 from .show_date_time import show_date_time
+from .handle_early_hints_response import HandleEarlyHintsResponse
 
-
-__all__ = ["ExtractParams", "ServeFile", "type_safe", "show_date_time"]
+__all__ = ["ExtractParams", "ServeFile", "type_safe", "show_date_time", "HandleEarlyHintsResponse"]
