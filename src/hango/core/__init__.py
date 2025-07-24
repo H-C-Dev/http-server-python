4e78d6e56d9139a86b75000d0ce805754d4a30ba
H-C-Dev <th.chan.dev2024@gmail.com>
2025-07-24 16:20:01 +0100

    Refactor: Created ServiceContainer for Dependecnies Injection pattern

--- src/hango/core/__init__.py
+++ src/hango/core/__init__.py
@@ -1,6 +1,7 @@
 from .config import SERVER_ROOT, STATIC_ROOT
 from .constants import CORS, http_status_codes_message, MethodType, ContentType, EarlyHintsClient, EXTENSION_TO_MIME
 from .type_safe import type_safe
+from .container import ServiceContainer
 __all__ = [
     "SERVER_ROOT",
     "STATIC_ROOT",
@@ -10,5 +11,6 @@ __all__ = [
     "ContentType",
     "EarlyHintsClient",
     "EXTENSION_TO_MIME"
-    "type_safe"
+    "type_safe",
+    "ServiceContainer"
 ]
