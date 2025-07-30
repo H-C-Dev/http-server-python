32f3c4256dee5e11d2d13a36f0775d03c6e9f3be
H-C-Dev <th.chan.dev2024@gmail.com>
2025-07-30 17:40:55 +0100

    Chore: Updated module name

--- src/hango/examples/handler.py
+++ src/hango/examples/handler.py
@@ -1,10 +1,10 @@
 from hango.http   import Response
 from hango.core  import type_safe
 import asyncio
-from hango.example_entry_point import app
+from hango.example_entry_point import server
 
 
-@app.set_global_middlewares
+@server.set_global_middlewares
 def foo_middleware(handler):
     async def wrapped(request):
         print("[Middleware] mw says hello to handler for:", request)
@@ -26,16 +26,16 @@ def local_middleware(handler):
     return wrapped
 
 
-@app.GET("/favicon.ico")
+@server.GET("/favicon.ico")
 def catch_favicon(request) -> Response:
     return Response(body="a favicon is detected", status_code="200")
 
-@app.GET("/test", local_middlewares=[local_middleware])
+@server.GET("/test", local_middlewares=[local_middleware])
 @type_safe
 def return_hello_world(request) -> Response:
     return Response(body="hello world", status_code="200")
 
-@app.GET("/data/{client_data}")
+@server.GET("/data/{client_data}")
 @type_safe
 def return_client_data(request) -> Response:
     res = test_safe("hello")
@@ -43,7 +43,7 @@ def return_client_data(request) -> Response:
     print(res)
     return Response(body=request.params, status_code="200")
 
-@app.POST("/post")
+@server.POST("/post")
 @type_safe
 def post_endpoint(request) -> Response:
     print("[Data received from client]:", request.body)
@@ -53,18 +53,18 @@ def post_endpoint(request) -> Response:
 def test_safe(foo: str) -> str:
     return foo
 
-@app.GET("/async-test")
+@server.GET("/async-test")
 @type_safe
 async def async_test_handler(request) -> Response:
     await asyncio.sleep(1)  
     return Response(body="this is from async function", status_code="200")
 
 
-@app.set_hook_after_each_handler
+@server.set_hook_after_each_handler
 def log_response(request, response):
     print('[HOOK RESPONSE]:',response)
 
-@app.set_hook_before_each_handler
+@server.set_hook_before_each_handler
 def log_request(request):
     print('[HOOK REQUEST]', request)
 
