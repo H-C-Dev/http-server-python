328620f80c520858351542590ab236bb7fa37b18
H-C-Dev <th.chan.dev2024@gmail.com>
2025-08-08 17:57:30 +0100

    Chore: Created the CacheHelper class to handle the cache handler

--- src/hango/middleware/__init__.py
+++ src/hango/middleware/__init__.py
@@ -1,5 +1,5 @@
 from .middleware_chain import MiddlewareChain
 from .cors import cors_middleware
-from .cache import cache_middleware
+from .cache import CacheHelper
 
-__all__ = ['MiddlewareChain', 'cors_middleware', 'cache_middleware']
+__all__ = ['MiddlewareChain', 'cors_middleware', 'CacheHelper']
