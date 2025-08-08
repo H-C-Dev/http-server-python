89e04d545f01901c305b29f00f10d40e535663e4
H-C-Dev <th.chan.dev2024@gmail.com>
2025-08-08 17:56:22 +0100

    Chore: Created is_coroutine function to check whether a handler is async while invoking the handler

--- src/hango/utils/__init__.py
+++ src/hango/utils/__init__.py
@@ -1,5 +1,6 @@
 from .path_utils import ExtractParams, ServeFile
 from .time_utils import response_time
+from .is_coroutine import is_coroutine
 # from .handle_early_hints_response import HandleEarlyHintsResponse
 
-__all__ = ["ExtractParams", "ServeFile", "show_date_time", "response_time"]
+__all__ = ["ExtractParams", "ServeFile", "show_date_time", "response_time", "is_coroutine"]
