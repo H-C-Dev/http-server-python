81416184c7e451c7ac07bc53d28a7975f5c3d4c4
H-C-Dev <th.chan.dev2024@gmail.com>
2025-08-10 23:13:48 +0100

    Chore: Added Rate_limit_middleware and RateLimiter class to __init__

--- src/hango/middleware/__init__.py
+++ src/hango/middleware/__init__.py
@@ -1,5 +1,6 @@
 from .middleware_chain import MiddlewareChain
 from .cors import cors_middleware
 from .cache import CacheHelper
+from .rate_limit_middleware import make_rate_limit_middleware, RateLimiter
 
-__all__ = ['MiddlewareChain', 'cors_middleware', 'CacheHelper']
+__all__ = ['MiddlewareChain', 'cors_middleware', 'CacheHelper', 'make_rate_limit_middleware', 'RateLimiter']
