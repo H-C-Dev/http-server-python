ef3f1510d2a1ce65ac9f51c0196e3c5314328e55
H-C-Dev <th.chan.dev2024@gmail.com>
2025-08-19 17:55:30 +0100

    Chore: Added is_cors_allowed

--- src/hango/middleware/__init__.py
+++ src/hango/middleware/__init__.py
@@ -1,7 +1,7 @@
 from .middleware_chain import MiddlewareChain
-from .cors import cors_middleware
+from .cors import cors_middleware, is_cors_allowed
 from .cache import CacheHelper
 from .rate_limit_middleware import make_rate_limit_middleware, RateLimiter
 from .type_validation_middleware import make_validate_middleware, Validator
 from .obs_middleware import make_obervabilty_middleware
-__all__ = ['MiddlewareChain', 'cors_middleware', 'CacheHelper', 'make_rate_limit_middleware', 'RateLimiter', 'make_validate_middleware', 'Validator']
+__all__ = ['MiddlewareChain', 'cors_middleware', 'CacheHelper', 'make_rate_limit_middleware', 'RateLimiter', 'make_validate_middleware', 'Validator', 'make_obervabilty_middleware', 'is_cors_allowed']
