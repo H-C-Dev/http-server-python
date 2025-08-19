4a9107f0ee64d05c97e16152bd171ff318d112ba
H-C-Dev <th.chan.dev2024@gmail.com>
2025-08-19 19:39:45 +0100

    Chore: Added SLOW_THREAD from core

--- src/hango/middleware/obs_middleware.py
+++ src/hango/middleware/obs_middleware.py
@@ -1,7 +1,8 @@
 
 from hango.utils import is_coroutine
 from hango.obs import start_request, end_request, inc_request, observe_latency
-def make_obervabilty_middleware(SLOW_THRESHOLD: int = 500):
+from hango.core import SLOW_THRESHOLD
+def make_obervabilty_middleware(SLOW_THRESHOLD: int = SLOW_THRESHOLD):
     def obervability_middleware(handler):
         from hango.custom_http import Request, Response
         async def wrapped(request: Request):
