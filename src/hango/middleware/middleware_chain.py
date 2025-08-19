6620676b4eab7d970a95fb6e6bc36e38943d515f
H-C-Dev <th.chan.dev2024@gmail.com>
2025-08-19 19:45:43 +0100

    Fix: Fixed syntax

--- src/hango/middleware/middleware_chain.py
+++ src/hango/middleware/middleware_chain.py
@@ -4,7 +4,7 @@ from .obs_middleware import make_obervabilty_middleware
 from .session_middleware import make_session_middleware
 from hango.session import SessionStore
 
-DEFAULT_MIDDLEWARES = [cors_middleware, make_obervabilty_middleware]
+DEFAULT_MIDDLEWARES = [cors_middleware, make_obervabilty_middleware()]
 
 class MiddlewareChain:
     def __init__(self, session_store: SessionStore=None):
